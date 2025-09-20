acf9628060af2b8ae2f3d98fb2062d9c61b18daf
valmaiimtiyaz <valmaityz30@gmail.com>
2025-09-21 05:00:11 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.getElementById("menu-toggle");
   const menu = document.getElementById("menu");
+
   if (toggle && menu) {
+    // toggle menu
     toggle.onclick = function () {
       menu.classList.toggle("show");
     };
+    // auto-close kalau klik link di menu
+    const links = menu.querySelectorAll("a");
+    links.forEach((link) => {
+      link.addEventListener("click", function () {
+        menu.classList.remove("show");
+      });
+    });
   }
 });
